3862157dfebb3794b06a7195e2251559c77387c4
dms98 <dms98@yahoo.com>
2016-09-20 15:45:30 -0300

    Add files via upload

---
+++ redir2.php
@@ -0,0 +1,143 @@
+<?php
+
+$par_par = $_REQUEST["par"];
+$par_go = $_GET["go"];
+
+include 'vai.php';
+
+$par_go = strtolower($par_go);
+
+$top_url= "http://<URL of your google firebase database>";
+
+if ($par_go!="") { 
+	
+	$valor = httpGet($top_url, $par_go);
+	if (is_numeric($valor)==false) {
+		$valor=0;
+	}
+	$valor++;
+	httpPut($top_url, $par_go, $valor);
+	}
+
+
+if (substr($par_go, 0, 8) == "listaabc") {
+	$par_go="listaabc";
+	header ('Cache-Control: no-cache, must-revalidate, Post-Check=0, Pre-Check=0');
+	//header ('Expires: Sat, 25 Dez 1997 05:00:00 GMT');
+	header ('Expires: -1');
+	header ('Pragma: no-cache');
+	//header ('X-Cacheable: no-cache');
+	//header ('X-Served-From-Cache: no-cache');
+
+	
+        echo '<HTML><BODY>';
+        echo '<HR>';
+	foreach($vai as $x => $x_value) {
+		echo 'Key= &nbsp;<B>' . $x . '</B>, Value= &nbsp;' . $x_value;
+		echo '<br>';
+	}
+        echo '<HR>';
+        echo '</BODY></HTML>';
+        
+         
+        $valor = httpGet($top_url, $par_go);
+        
+        echo 'antes: ' . $valor . '<BR>';
+        
+        $valor++;
+        
+        echo '<BR>depois: ' . $valor . '<BR>';
+        
+        
+        echo '<BR>re:' . httpPut($top_url, $par_go, $valor);
+            
+        $par_go="";
+        die;
+}
+
+if ($par_go!="") { 
+	
+	$vai_para = "Location: " . $vai[$par_go];
+	header($vai_para);
+	die;
+	}
+	
+
+if ($par_go=="a") {
+	//header("Location: http://www.mp.go.gov.br");
+	echo 'par_par: ' . $par_par . '<br>';
+	echo 'get-par: ' . $_GET["par"] . '<br>';
+	echo 'par_go: ' . $par_go . '<br>';
+	echo 'get_go: ' . $_GET["go"] . '<br>';
+	echo '<HR>vai para: ' . $vai[$par_go] . '<BR>';
+	
+	if ($par_par=="lista") { 
+		foreach($vai as $x => $x_value) {
+			echo "Key=" . $x . ", Value=" . $x_value;
+			echo "<br>";
+		}
+	}
+	die;
+}
+
+
+function envia ($url, $par_go, $dados)
+{
+
+	$postdata = http_build_query(
+	    array(
+	        $par_go => $dados
+	    )
+	);
+	//$postdata=$dados;
+	
+	//echo '<BR>post-data:' . $postdata;
+ 
+	$opts = array('http' =>
+	    array(
+	        'method'  => 'POST',
+	        'header'  => 'Content-Type: application/json\r\n'
+	         . "Content-Length: " . strlen($postdata) . "\r\n",
+	        'content' => $postdata
+	    )
+	);
+	
+	//echo '<BR>opts: ' . var_dump($opts);
+ 
+	$context  = stream_context_create($opts);
+
+	//echo '<BR>ctx: ' . var_dump($context);
+	
+	$resultado =  file_get_contents($url, false, $contexto);
+    
+    return $resultado;
+    
+}
+
+function httpPut($top_url, $parametro, $data)
+{
+    $url = $top_url . $parametro . ".json";
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    //curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
+
+function httpGet($top_url, $parametro) {
+	// pega dados da WEB
+	
+	$url = $top_url . $parametro . ".json";
+        
+        $valor = file_get_contents($url);
+        
+        return $valor;
+	
+}
+
+
+?>
---
+++ vai.php
@@ -0,0 +1,39 @@
+<?php
+
+// Redirecionamentos principais
+$vai['advcash']="http://wallet.advcash.com/referral/1f1c91f0-7783-4e91-b020-3473ecfeead7";
+$vai['amazon']="http://goo.gl/F0JxB"; // Link geral Amazon - neoageinform-20
+$vai['ali']="http://s.click.aliexpress.com/e/qFEE2VnI6"; // Aliexpress
+$vai['aliexpress']="http://s.click.aliexpress.com/e/qFEE2VnI6"; // Aliexpress
+$vai['cab']="http://cabify.com/i/daniels1396";
+$vai['cabify']="http://cabify.com/i/daniels1396";
+$vai['dropbox']="http://goo.gl/nEIj";
+$vai['dmslabs']="http://goo.gl/GZLCBN"; // Dmslabs
+$vai['android']="http://goo.gl/GZLCBN"; // Dmslabs
+$vai['dx']="http://goo.gl/dNp3X"; // DealExtreme
+$vai['payeer']="https://payeer.com/096109";
+$vai['payoneer']="https://goo.gl/xAXruf";
+$vai['shipito']="http://goo.gl/EzZul";
+$vai['skrill']="http://www.skrill.com";  // atualizar o código - - - - - - -
+$vai['netspend']="http://goo.gl/hmTQb";
+$vai['neteller']="http://wlneteller.adsrv.eacdn.com/C.ashx?btag=a_3238b_95c_&affid=2972&siteid=3238&adid=95";
+$vai['netellerfra']="http://wlneteller.adsrv.eacdn.com/I.ashx?btag=a_3238b_2595c_&affid=2972&siteid=3238&adid=2595";          
+$vai['netellerscr']="http://wlneteller.adsrv.eacdn.com/S.ashx?btag=a_3238b_2593c_&affid=2972&siteid=3238&adid=2593";
+$vai['123']="http://";                   
+$vai['img']="http://wlneteller.eacdn.com/wlneteller/img/new2015vip_300x250portuguese.gif";       
+$vai['hotel']="http://www.booking.com/index.html?aid=362375";
+$vai['booking']="http://www.booking.com/index.html?aid=362375";
+$vai['uber']="https://www.uber.com/invite/nqsk9zs4ue"; // 
+$vai['transferwise']="https://transferwise.com/u/ef9ec1"; 
+$vai['iherb']="http://www.iherb.com/iherb-brands?rcode=QGT250"; //10% 
+$vai['iherbfree']="http://www.iherb.com/free/?rcode=QGT250";  // free item. US$5,00
+
+
+
+// Redirecionamentos alternativos
+$vai['chromecast']="http://www.amazon.com/s/ref=as_li_ss_tl?url=search-alias%3Daps&field-keywords=chromecast&linkCode=sl2&tag=neobr-20&linkId=54355213dae2cbdf801a4ee72e4707a1";  // pesquisa Chromecast Generica NEO-BR
+
+
+
+
+?>
